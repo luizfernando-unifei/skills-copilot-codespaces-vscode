6f1ebd63957f80d98bbf4f6c1e33797f9e5dc929
Luiz Fernando Nunes <luizfernandolfn@unifei.edu.br>
2025-02-07 13:46:40 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,34 +1,17 @@
 //create web server
-//create a web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const { parse } = require('querystring');
-const { parse: parseUrl } = require('url');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var request = require('request');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-    const { url } = req;
-    if (url === '/') {
-        fs.createReadStream(path.resolve(__dirname, 'index.html')).pipe(res);
-    } else if (url === '/comment' && req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const { comment } = parse(body);
-            fs.appendFileSync(path.resolve(__dirname, 'comments.txt'), `${comment}\n`);
-            res.end();
-        });
-    } else if (url === '/comments' && req.method === 'GET') {
-        const comments = fs.readFileSync(path.resolve(__dirname, 'comments.txt'), 'utf8');
-        res.end(comments);
-    } else {
-        res.statusCode = 404;
-        res.end();
-    }
-});
+var comments = [];
+var commentsPath = path.resolve(__dirname, 'comments.json');
 
-server.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
