cd840684690abbe755347568b51ca73c96c0838e
Luiz Fernando Nunes <luizfernandolfn@unifei.edu.br>
2025-02-07 13:24:33 +0000

    Copilot secont commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return [
+    {
+      name: "John Doe",
+      skills: ["HTML", "CSS", "JS", "React"],
+    },
+    {
+      name: "Jane Doe",
+      skills: ["Node", "Express", "MongoDB", "React"],
+    },
+    {
+      name: "Jenny Doe",
+      skills: ["HTML", "CSS", "JS", "React"],
+    },
+  ];
+}
